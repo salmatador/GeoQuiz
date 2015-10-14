da9b613c744fca3850ace5107b3f5f1bd811f378
salmatador <salmatador@moonstub.com>
2015-10-14 07:42:37 -0700

    Complete GeoQuiz Application minus the actual Geo Questions

--- app/src/main/java/app/training/example/com/geoquiz/CheatActivity.java
+++ app/src/main/java/app/training/example/com/geoquiz/CheatActivity.java
@@ -1,5 +1,7 @@
 package app.training.example.com.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,11 +11,22 @@ import android.widget.TextView;
 public class CheatActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = CheatActivity.class.getSimpleName();
-    public static final String EXTRA_ANSWER_IS_TRUE = "app.training.example.com.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_IS_TRUE = "app.training.example.com.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "app.training.example.com.geoquiz.answer_shown";
 
     private boolean mAnswerIsTrue;
     private Button mShowAnswerButton;
     private TextView mAnswerTextView;
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue){
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
+
+    public static boolean wasAnswerShown(Intent result){
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +43,14 @@ public class CheatActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //If true set text to true if false set text to false
                 mAnswerTextView.setText((mAnswerIsTrue) ? R.string.true_button_text : R.string.false_button_text);
+                setAnswerShownResult(true);
             }
         });
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
     }
 }
--- app/src/main/java/app/training/example/com/geoquiz/QuizActivity.java
+++ app/src/main/java/app/training/example/com/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package app.training.example.com.geoquiz;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +10,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+// git checkout -b [branch name]
+// git commit -a -m ["Commit Message"]
+// git push --set-upstream origin [branch name]
+// git checkout master
+// git push
 public class QuizActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = QuizActivity.class.getSimpleName();
     private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -22,26 +29,33 @@ public class QuizActivity extends AppCompatActivity {
 
     private TextView mQuestionTextView;
     private int mCurrentIndex = 0;
-    private Question[] mQuestionBank = new Question[] {
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_1, true),
             new Question(R.string.question_2, true),
             new Question(R.string.question_3, true),
             new Question(R.string.question_4, true),
             new Question(R.string.question_5, true)
     };
+    private boolean mIsCheater;
 
-    private void checkAnswer(boolean userPressedTrue){
+    private void checkAnswer(boolean userPressedTrue) {
+
         boolean isAnswerTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-        int messageId = 0 ;
-        if(userPressedTrue == isAnswerTrue){
-            messageId = R.string.correct_toast;
+        int messageId = 0;
+        if (mIsCheater) {
+            messageId = R.string.judgment_toast;
         } else {
-            messageId = R.string.incorrect_toast;
+            if (userPressedTrue == isAnswerTrue) {
+                messageId = R.string.correct_toast;
+            } else {
+                messageId = R.string.incorrect_toast;
+            }
         }
         Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
     }
 
-    private void updateQuestion(){
+    private void updateQuestion() {
+        mIsCheater = false;
         int question = mQuestionBank[mCurrentIndex].getQuestion();
         mQuestionTextView.setText(question);
     }
@@ -59,8 +73,9 @@ public class QuizActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "onCreate(Bundle) called");
 
         setContentView(R.layout.activity_quiz);
+        getSupportActionBar().setSubtitle(R.string.subtitle_text);
 
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
 
@@ -106,11 +121,23 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
-                intent.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
-                startActivity(intent);
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
     }
 
     @Override
